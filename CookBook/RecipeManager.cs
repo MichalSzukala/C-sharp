02b36d1536f89d5d59d4d45054c5d5ab7a719dd9
Michal Szukala <szukalagithub@gmail.com>
2017-11-17 23:40:46 +0100

    recipe manager class

--- CookBook/RecipeManager.cs
+++ CookBook/RecipeManager.cs
@@ -18,5 +18,60 @@ namespace CookBook
             recipeList = new Recipe[maxNumOfElements];
         }
 
+        //Assigning properties for the RecipeManager class objects
+        public Recipe[] RecipeList
+
+        {
+            get { return recipeList; }
+            set { recipeList = value; }
+        }
+
+        //Will return first empty position in the recipeList array
+        private int FindVacantPosition()
+        {
+            for (int i = 0; i < recipeList.Length; i++)
+            {
+                if (recipeList[i] == null)
+                    return i;
+            }
+            return -1;
+        }
+
+        //will save recipe object into recipeList array if there is free space, and return true/false
+        public bool Add(Recipe newRecipe)
+        {
+            bool ok = true;
+            int emptyIndex = FindVacantPosition();
+
+            if (emptyIndex >= 0 && newRecipe != null)
+            {
+
+                recipeList[emptyIndex] = newRecipe;
+            }
+            else
+            {
+                MessageBox.Show("Recipe's memory is full", "Error");
+                ok = false;
+            }
+
+            return ok;
+
+        }
+
+        //overloaded Add method 
+        public bool Add(Recipe newRecipe, int index)
+        {
+            bool ok = true;
+
+            if (newRecipe != null)
+            {
+
+                recipeList[index] = newRecipe;
+            }
+            else
+                ok = false;
+            return ok;
+
+        }
     }
 }
