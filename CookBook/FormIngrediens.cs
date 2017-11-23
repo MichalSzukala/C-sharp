37e7fd26ee7ce99d4aa209a8401b7a55cada3ec8
Michal Szukala <szukalagithub@gmail.com>
2017-11-23 23:18:20 +0100

    ingrediens class

--- CookBook/FormIngrediens.cs
+++ CookBook/FormIngrediens.cs
@@ -166,7 +166,28 @@ namespace CookBook
 
         }
 
-        
-        
+        //after clicking on the ListBox with recipes, the details will be shown in the input fields
+        private void listBoxListOfIngrediens_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxListOfIngrediens.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                textBoxPutIngredient.Text = ingredientsArray[selectedIndex];
+            }
+
+        }
+
+        ////will unselect selected ingredient
+        private void buttonUnselect_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxListOfIngrediens.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                listBoxListOfIngrediens.ClearSelected();
+                textBoxPutIngredient.Text = string.Empty;
+            }
+        }
     }
 }
